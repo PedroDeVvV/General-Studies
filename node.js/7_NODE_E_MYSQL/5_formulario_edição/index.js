f6e2ff58685cb6f92ca3b78ad461d59a25067ab6
PedroDeVvV <pedrohgs2004@gmail.com>
2024-01-30 13:29:45 -0300

    30-01-2024

---
+++ node.js/7_NODE_E_MYSQL/5_formulario_edição/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const exphs = require("express-handlebars");
+const mysql = require("mysql");
+const port = 3000;
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.engine("handlebars", exphs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+// rotas
+
+app.get("/books/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const book = data[0];
+
+    res.render("editbooks", { book });
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const book = data[0];
+
+    res.render("book", { book });
+  });
+});
+
+app.get("/books", (req, res) => {
+  const query = "SELECT * FROM books";
+
+  conn.query(query, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+    console.log(books);
+    res.render("books", { books });
+  });
+});
+
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pagesqty;
+
+  const query = `INSERT INTO books(title, pageqty) VALUES('${title}', '${pageqty}')`;
+
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect("/books");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "estudos",
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("Conectou ao MySQL");
+
+  app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+  });
+});
